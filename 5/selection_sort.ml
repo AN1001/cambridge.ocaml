4fd93b52194ca5c83e657f415f8a5e7c7994aae5
Arnav Nagpure <81749717+AN1001@users.noreply.github.com>
2025-10-31 15:14:41 +0000

    Create selection_sort.ml

---
+++ 5/selection_sort.ml
@@ -0,0 +1,17 @@
+let rec find_minimal cur_min = function
+  | x::xs -> if x < cur_min
+             then find_minimal x xs
+             else find_minimal cur_min xs
+  | [] -> cur_min
+;;
+let rec remove_el el = function
+  | [] -> []
+  | x::xs -> if x = el then xs else x::(remove_el el xs)
+;;
+let rec selection_sort list =
+  match list with
+    | [] -> []
+    | x::xs -> let minimum = find_minimal x xs in
+               minimum::selection_sort (remove_el minimum (x::xs))
+;;
+selection_sort [7;6;5;4;3;2;1]
