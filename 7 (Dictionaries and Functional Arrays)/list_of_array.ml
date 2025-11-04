aa863ba458eada56dbf56dc57e5d451635dddcb5
Arnav Nagpure <81749717+AN1001@users.noreply.github.com>
2025-11-04 21:54:46 +0000

    Update list_of_array.ml

--- 7 (Dictionaries and Functional Arrays)/list_of_array.ml
+++ 7 (Dictionaries and Functional Arrays)/list_of_array.ml
@@ -1,3 +1,4 @@
+type 'a tree = Lf | Br of 'a * 'a tree * 'a tree
 let rec merge l1 l2 =
 match l1, l2 with
   | x::xs, y::ys -> x::y::merge xs ys
@@ -8,3 +9,7 @@ match l1, l2 with
 let rec listofarray = function
   | Lf -> []
   | Br (v,lt,rt) -> v::merge (listofarray lt) (listofarray rt)
+;;
+
+(*Example Case*)
+listofarray (Br (1,Br(2,Lf,Lf),Br(3,Lf,Lf)))
