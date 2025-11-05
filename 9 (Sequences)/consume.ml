a3f977bcafe4030468e2750946f9fa3fc08f0643
Arnav Nagpure <81749717+AN1001@users.noreply.github.com>
2025-11-05 15:11:27 +0000

    Create consume.ml

---
+++ 9 (Sequences)/consume.ml
@@ -0,0 +1,17 @@
+(*Consumes the first n terms of a sequence*)
+exception EndOfSeq
+type 'a seq = Nil | Cons of 'a * (unit -> 'a seq)
+let hd = function
+  | Nil -> raise EndOfSeq
+  | Cons (v,_) -> v
+let next = function
+  | Nil -> raise EndOfSeq
+  | Cons (_,tail) -> tail ()
+let rec get n s = match n,s with
+  | 0,_ -> []
+  | _,Nil -> []
+  | _,xf -> hd xf :: get (n-1) (next xf)
+let rec from k = Cons (k, (fun () -> from (k+1)))
+;;
+(*Example case*)
+get 5 (from 5)
