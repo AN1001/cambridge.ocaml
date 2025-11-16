73de049d7e0831a321200ae6f3d7c3b7b9f22cbf
Arnav Nagpure <81749717+AN1001@users.noreply.github.com>
2025-11-16 20:55:03 +0000

    Create breadth.ml

---
+++ 10 (Queues)/breadth.ml
@@ -0,0 +1,28 @@
+exception EmptyQ
+type 'a queue = Q of 'a list * 'a list
+type 'a tree = Lf | Br of 'a * 'a tree * 'a tree
+
+let normalise = function
+  | Q ([], tail) -> Q(List.rev tail, [])
+  | q -> q
+
+let q_is_null = function
+  | Q ([],[]) -> true
+  | q -> false
+
+let enq x = function
+  | Q (front, back) -> normalise (Q (front, x::back))
+
+let deq = function
+  | Q ([],_) -> raise EmptyQ
+  | Q (x::front, back) -> normalise (Q (front, back))
+
+let q_head = function
+  | Q ([],_) -> raise EmptyQ
+  | Q (x::front, back) -> x
+
+let rec breadth = function
+  | Q (Br (x, lt, rt)::front, back) -> x::(breadth (normalise (Q(front, lt::rt::back))))
+  | Q (Lf::front, back) -> breadth (normalise (Q(front, back)))
+  | q_empty -> []
+;;
