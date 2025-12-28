6416981f8e5bbf9fe626b1e2ad697ba20a9a3860
Arnav Nagpure <81749717+AN1001@users.noreply.github.com>
2025-12-28 13:25:35 +0000

    Create encode_in_place.ml

---
+++ Exercises/encode_in_place.ml
@@ -0,0 +1,14 @@
+type 'a rle = 
+  | One of 'a
+  | Many of int * 'a
+
+let encode l = 
+let rec aux count acc = function
+  | x::y::xs -> if x=y then aux (count+1) acc (y::xs)
+                       else aux 0 ((if count=0 then One(x) 
+                                              else Many(count+1, x))::acc) (y::xs)
+  | [x] ->  (if count=0 then One(x) else Many(count+1, x))::acc
+  | [] -> acc
+in List.rev (aux 0 [] l)
+;;
+encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"]
