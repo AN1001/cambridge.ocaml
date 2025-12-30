8bd1e8953c7b0b95961b0dbfb2fcc49293164752
Arnav Nagpure <81749717+AN1001@users.noreply.github.com>
2025-12-30 13:35:15 +0000

    Update encode_in_place.ml

--- Exercises/encode_in_place.ml
+++ Exercises/encode_in_place.ml
@@ -6,7 +6,7 @@ let encode l =
 let rec aux count acc = function
   | x::y::xs -> if x=y then aux (count+1) acc (y::xs)
                        else aux 0 ((if count=0 then One(x) 
-                                              else Many(count+1, x))::acc) (y::xs)
+                                               else Many(count+1, x))::acc) (y::xs)
   | [x] ->  (if count=0 then One(x) else Many(count+1, x))::acc
   | [] -> acc
 in List.rev (aux 0 [] l)
